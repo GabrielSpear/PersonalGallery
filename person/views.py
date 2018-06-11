b1bba973a4c775d7f4b1dee40cf4fa86c2d29709
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 23:10:51 +0400

    Views bug now works

--- person/views.py
+++ person/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render, Http404
 from .models import Photos
 
 # Create your views here.
-
-def home(request):
-    image = Image.get_images()
-    return render(request,'index.html',{"images":image})
-
 def my_gallery(request):
     photos = Photos.objects.all()
     return render(request, 'gallery.html', {'photos':photos})
