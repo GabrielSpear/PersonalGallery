c53a7883e79820a845bfe76c2973ca03d7f981e9
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 18:59:06 +0400

    Location function

--- person/models.py
+++ person/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
