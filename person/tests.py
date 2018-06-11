f84176a21d3d036dd68d16e33a4918b81b9b6dac
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 19:32:39 +0400

    Location Test Added

--- person/tests.py
+++ person/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+import datetime as dt
 # Create your tests here.
+from .models import Photos, categories, Location
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)
