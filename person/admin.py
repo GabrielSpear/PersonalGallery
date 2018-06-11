d03006b3c8c5621d8ca58deedcc8759b20799209
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 18:57:32 +0400

    Loc photo, category register

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+class PhotosAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
+
+admin.site.register(Photos, PhotosAdmin)
+admin.site.register(Location)
+admin.site.register(categories)
