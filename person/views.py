8c8821926fd6e9d2b45e7a9779d8a3f16168cda0
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 21:27:46 +0400

    single photo view

--- person/views.py
+++ person/views.py
@@ -10,3 +10,7 @@ def home(request):
 def my_gallery(request):
     photos = Photos.objects.all()
     return render(request, 'gallery.html', {'photos':photos})
+
+def single_photo(request, photo_id):
+    photo = Photos.objects.get(id=photo_id)
+    return render(request, 'photo.html', {'photo':photo})
