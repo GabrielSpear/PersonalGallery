8c513281a07ddf0fb1c7e40a9315ce086f9e4d66
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 21:23:25 +0400

    Home view function

--- person/views.py
+++ person/views.py
@@ -1,34 +1,8 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from .models import Image
+from django.shortcuts import render, Http404
+from .models import Photos
 
 # Create your views here.
 
 def home(request):
     image = Image.get_images()
     return render(request,'index.html',{"images":image})
-
-def search(request):
-        if 'category' in request.GET and request.GET["category"]:
-            search_term = request.GET.get("category")
-            searched_category = Image.search_by_category(search_term)
-            message = f"{search_term}"
-            return render(request,'search.html',{"message":message,"categories":searched_category})
-
-
-        else:
-            message = "You havent searched any category"
-            return render(request,'search.html',{"message":message})
-
-
-def image(request,image_id):
-    try:
-        image=Image.objects.get(id=image_id)
-    except DoesNotExist:
-        raise Http404()
-    return render(request,'image.html',{"image":image})
-
-
-def filter(request):
-    image = Image.get_images()
-    return render(request,'location.html',{"images":image})
