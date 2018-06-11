1e8dc0295818e0512a54ab3318070fcdb575ec7a
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 20:10:42 +0400

    search

--- person/views.py
+++ person/views.py
@@ -7,3 +7,15 @@ from .models import Image
 def home(request):
     image = Image.get_images()
     return render(request,'index.html',{"images":image})
+
+def search(request):
+        if 'category' in request.GET and request.GET["category"]:
+            search_term = request.GET.get("category")
+            searched_category = Image.search_by_category(search_term)
+            message = f"{search_term}"
+            return render(request,'search.html',{"message":message,"categories":searched_category})
+
+
+        else:
+            message = "You havent searched any category"
+            return render(request,'search.html',{"message":message})
