57cd021731a1cc2fc50f649c4761626b1bbcd617
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-11 18:59:53 +0400

    Photo category function

--- person/models.py
+++ person/models.py
@@ -13,3 +13,15 @@ class Location(models.Model):
 
     def delete_location(self):
         self.delete()
+
+class categories(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
